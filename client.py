f9c490f0d9a7749f102cb68ae4ee631d12d732a6
Alexanderskg <alexanderguerre95@hotmail.com>
2024-05-29 21:31:12 +0100

    changes1

--- client.py
+++ client.py
@@ -1,14 +1,26 @@
-import socket 
+import socket
+import time
+client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-socket.connect((socket.gethostname(), 5050)) #change ip
+#host = '192.168.122.165'
+host = socket.gethostname() 
+port= 5050
 
-message = ''
+client_socket.connect((host, port))
+
+message_recv = ''
+message_send = "Hello server" 
 
 while True:
-    msg = socket.recv(8)
+    msg = client_socket.recv(16)
     if len(msg) <= 0:
         break
-    message += msg.decode("utf-8")
+    message_recv += msg.decode("utf-8" )
+    print(message_recv)
+    
+    #time.sleep(1)
+    client_socket.send(message_send.encode("utf-8"))
+    
 
-print(msg.decode("utf-8"))
+client_socket.close()
+
--- server.py
+++ server.py
@@ -1,14 +1,35 @@
-import socket 
+import socket
+import time
 
-socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-socket.bind((socket.gethostname()),5050) #change ip
-socket.listen(5)
+server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+host = socket.gethostname() 
+#host = '192.168.122.165'
+port = 5050
+
+server_socket.bind((host, port))
+
+server_socket. listen (5)
+print(f"Connection with server established")
+
+
+message_send = "Hello client"
 
 while True:
-    clientsocket, address = socket.accept()
-    print(f"Connection with server established!")
-    clientsocket.send(bytes("Test string") , "utf-8")
+
+    clientsocket, address = server_socket.accept()
+    clientsocket.send (message_send.encode("utf-8"))
+
+    #time.sleep(1)
+    message_recv = ''
+    msg= clientsocket.recv(16)
+    if len(msg)<=0:
+        break
+    message_recv += msg.decode("utf-8")
+
+    print(message_recv)
+
+
     clientsocket.close()
-
-
+    
     
